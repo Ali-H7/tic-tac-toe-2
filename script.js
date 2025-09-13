e876b886e60d0475ab89ed19b88ad7c55f9202c2
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-09-13 19:45:17 +0300

    Refactor game object

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const game = (function () {
-    const gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    const winningCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    let currentTurn = 1;
 
-    function printBoard() {
-        console.log(gameBoard[0], gameBoard[1], gameBoard[2]);
-        console.log(gameBoard[3], gameBoard[4], gameBoard[5]);
-        console.log(gameBoard[6], gameBoard[7], gameBoard[8]);
+    const board = (function () {
+        const gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const update = function (location, mark) {
+            gameBoard[location] = mark;
+        };
+        const print = function () {
+            console.log(gameBoard[0], gameBoard[1], gameBoard[2]);
+            console.log(gameBoard[3], gameBoard[4], gameBoard[5]);
+            console.log(gameBoard[6], gameBoard[7], gameBoard[8]);
+        };
+        return { gameboard, update, print };
+    })();
+
+    const turn = function () {
+        let currentTurn = 1;
+        let currentPlayer;
+        const nextTurn = () => currentTurn++;
+        const resetTurn = () => currentTurn = 1;
+        const getTurn = () => currentTurn;
+        const updatePlayer = (player) => currentPlayer = player;
+        return { nextTurn, resetTurn, getTurn, updatePlayer };
     };
-
-    function updateBoard(location, mark) {
-        gameBoard[location] = mark;
-    };
-
-    function nextTurn() {
-        currentTurn++;
-    }
 
     // check winning condition need work
     function checkGameStatus(playerName, playerMoves) {
-        const checkWinCondition = winningCombination.some(array => {
-            return array.every(number => playerMoves.includes(number));
-        })
         if (checkWinCondition) {
             console.log(`${playerName} wins"`);
         } else if (currentTurn === 9) {
@@ -29,7 +32,7 @@ const game = (function () {
         }
     };
 
-    return { gameBoard, printBoard, updateBoard, nextTurn, checkGameStatus };
+    return { gameBoard, printBoard, updateBoard, getCurrentTurn, nextTurn, checkGameStatus };
 })();
 
 function player(playerName, playerMark) {
@@ -60,8 +63,24 @@ function player(playerName, playerMark) {
         playerMoves.push(location);
     };
 
+    function winCondition() {
+        const winningCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        return winningCombination.some(array => array.every(number => playerMoves.includes(number)));
+    };
 
-    return { makeMove, getPlayerMark, getScore, inceraseScore };
+    function checkIfWon() {
+        const currentTurn = game.getCurrentTurn();
+        if (winCondition()) {
+            console.log(`${playerName} wins"`);
+            return true;
+        } else if (currentTurn === 9) {
+            console.log("Tie");
+            return false;
+        }
+    };
+
+
+    return { makeMove, getPlayerMark, getScore, inceraseScore, checkIfWon };
 };
 
 const logicController = (function () {
@@ -69,6 +88,9 @@ const logicController = (function () {
         const playerMark = player.getPlayerMark();
         game.updateBoard(location, playerMark)
         player.makeMove(location);
+        const checkResult = player.checkIfWon();
+        // need some work here
+        if (checkResult) endGame(winner,)
         // game.checkGameStatus(playerName, playerMoves);
         game.nextTurn();
         game.printBoard();
